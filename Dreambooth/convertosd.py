e34a4aed90959605f8a602181acb20b9db2f32a8
Ben <110583491+TheLastBen@users.noreply.github.com>
2022-11-04 07:36:48 +0100

    Update convertosd.py

--- Dreambooth/convertosd.py
+++ Dreambooth/convertosd.py
@@ -177,10 +177,10 @@ def convert_vae_state_dict(vae_state_dict):
             mapping[k] = v
     new_state_dict = {v: vae_state_dict[k] for k, v in mapping.items()}
     weights_to_convert = ["q", "k", "v", "proj_out"]
+    print("Converting to CKPT ...")    
     for k, v in new_state_dict.items():
         for weight_name in weights_to_convert:
             if f"mid.attn_1.{weight_name}.weight" in k:
-                print(f"Reshaping {k} for SD format")
                 new_state_dict[k] = reshape_weight_for_sd(v)
     return new_state_dict
 
